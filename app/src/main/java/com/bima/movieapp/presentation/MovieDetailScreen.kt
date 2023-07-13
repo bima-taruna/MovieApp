919064b93bfa85aaabb65c652f26c97980094288
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-13 14:45:18 +0700

    fixing layout with combining row and constraint layout

--- app/src/main/java/com/bima/movieapp/presentation/MovieDetailScreen.kt
+++ app/src/main/java/com/bima/movieapp/presentation/MovieDetailScreen.kt
@@ -2,6 +2,7 @@
 
 package com.bima.movieapp.presentation
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -9,15 +10,23 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,6 +37,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bima.movieapp.common.Constant
@@ -45,59 +56,59 @@ fun MovieDetailScreen(
     viewModel: MovieDetailViewModel = hiltViewModel(),
 ) {
     val state = viewModel.state.value
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.background)) {
-    state.movie?.let { movie->
-        Column(modifier = Modifier, Arrangement.Center) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        state.movie?.let { movie ->
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
 
-//            Box(modifier = Modifier) {
-//                ImageBackdrop(imageUrl = Constant.IMG_URL + movie.backdropPath)
-//                Box(modifier = Modifier.fillMaxWidth().padding(top = 120.dp)) {
-//                    Row() {
-//                        GlideImage(model = Constant.IMG_URL_POSTER + movie.posterPath, contentDescription = "poster")
-//                        Box(modifier = Modifier.fillMaxWidth().height(200.dp)) {
-//                            Text(text = movie.title, modifier = Modifier.align(Alignment.BottomStart), style = MaterialTheme.typography.headlineSmall)
-//                        }
-//                    }
-//                }
-//            }
-        ConstraintLayout() {
-            val (backDrop,poster,title,overview) = createRefs()
-            GlideImage(
-                model = Constant.IMG_URL + movie.backdropPath,
-                contentDescription = "backdrop",
-                modifier = Modifier.fillMaxWidth().constrainAs(backDrop) {}
-            )
-            GlideImage(
-                model = Constant.IMG_URL_POSTER + movie.posterPath,
-                contentDescription = "poster",
-                modifier = Modifier.height(150.dp).padding(start = 24.dp).constrainAs(poster) {
-                    top.linkTo(title.top)
-                    start.linkTo(backDrop.start)
-                    bottom.linkTo(title.bottom)
-                }.border(width = 1.dp, color = MaterialTheme.colorScheme.onPrimaryContainer, shape = RoundedCornerShape(10.dp)),
+                ConstraintLayout() {
+                    val (backDrop, poster, title, overview) = createRefs()
+                    val horizontalChain =
+                        createHorizontalChain(poster, title, chainStyle = ChainStyle.Spread)
+                    GlideImage(
+                        model = Constant.IMG_URL + movie.backdropPath,
+                        contentDescription = "backdrop",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .constrainAs(backDrop) {}
+                    )
+                    Spacer(modifier = Modifier
+                        .padding(8.dp)
+                        .constrainAs(title) {
+                            top.linkTo(backDrop.bottom)
+                        })
+                    Row(modifier = Modifier.constrainAs(poster) {
+                        start.linkTo(parent.start)
+                        top.linkTo(title.top)
+                        bottom.linkTo(backDrop.bottom)
+                    }) {
+                        GlideImage(
+                            model = Constant.IMG_URL_POSTER + movie.posterPath,
+                            contentDescription = "poster",
+                            modifier = Modifier
+                                .height(150.dp)
+                                .padding(start = 24.dp),
 
-            )
-
-            Text(
-                text = movie.title,
-                style = MaterialTheme.typography.titleLarge,
-                maxLines = 4,
-                modifier = Modifier.padding(8.dp).width(200.dp).constrainAs(title) {
-                    top.linkTo(backDrop.bottom)
-                    start.linkTo(poster.end)
-                } )
-
-            Spacer(modifier = Modifier.padding(16.dp))
-            Text(text = movie.overview,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.constrainAs(overview){
-                    top.linkTo(poster.bottom)
-                })
+                            )
+                        Box(modifier = Modifier.fillMaxHeight()) {
+                            Text(
+                                text = movie.title,
+                                style = MaterialTheme.typography.titleLarge,
+                                maxLines = 4,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier
+                                    .padding( start = 8.dp)
+                                    .fillMaxWidth()
+                                    .align(Alignment.BottomCenter),
+                                )
+                        }
+                    }
+                }
+            }
         }
-        }
-    }
         if (state.error.isNotBlank()) {
             Text(
                 text = state.error,
