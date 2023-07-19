24c90063fc6cec6abeebeb382e99c8da1382beff
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-19 14:39:11 +0700

    add movie cast apiService

--- app/src/main/java/com/bima/movieapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/bima/movieapp/data/remote/retrofit/ApiService.kt
@@ -1,5 +1,6 @@
 package com.bima.movieapp.data.remote.retrofit
 
+import com.bima.movieapp.data.remote.dto.movieCastDto.MovieCastDto
 import com.bima.movieapp.data.remote.dto.movieDetailDto.MovieDetailDto
 import com.bima.movieapp.data.remote.dto.movieReviewsDto.MovieReviewsDto
 import com.bima.movieapp.data.remote.dto.nowPlayingDto.NowPlayingDto
@@ -19,4 +20,9 @@ interface ApiService {
     suspend fun getMovieReviews(
         @Path("movie_id") movieId:String
     ) : MovieReviewsDto
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCast(
+        @Path("movie_id") movieId: String
+    ) : MovieCastDto
 }
