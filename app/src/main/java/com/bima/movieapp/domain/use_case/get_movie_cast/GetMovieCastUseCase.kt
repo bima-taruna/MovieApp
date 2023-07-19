db2fc9f55e232127de675cb6a2c9145d7eaf72c6
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-19 15:01:43 +0700

    add movie cast use case

---
+++ app/src/main/java/com/bima/movieapp/domain/use_case/get_movie_cast/GetMovieCastUseCase.kt
@@ -0,0 +1,16 @@
+package com.bima.movieapp.domain.use_case.get_movie_cast
+
+import com.bima.movieapp.common.Resource
+import com.bima.movieapp.domain.model.Cast
+import com.bima.movieapp.domain.model.Movie
+import com.bima.movieapp.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMovieCastUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    operator fun invoke(movieId:String): Flow<Resource<List<Cast>>> {
+        return repository.getMovieCast(movieId)
+    }
+}
