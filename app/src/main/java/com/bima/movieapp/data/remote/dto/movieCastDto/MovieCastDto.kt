b9e623596464559534e5acd482d9144846d481b2
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-19 14:53:00 +0700

    map movie cast dto to movie cast model

--- app/src/main/java/com/bima/movieapp/data/remote/dto/movieCastDto/MovieCastDto.kt
+++ app/src/main/java/com/bima/movieapp/data/remote/dto/movieCastDto/MovieCastDto.kt
@@ -1,7 +1,10 @@
 package com.bima.movieapp.data.remote.dto.movieCastDto
 
 
+import com.bima.movieapp.data.remote.dto.nowPlayingDto.NowPlayingDto
+import com.bima.movieapp.domain.model.NowPlaying
 import com.google.gson.annotations.SerializedName
+import com.bima.movieapp.domain.model.Cast
 
 data class MovieCastDto(
     @SerializedName("cast")
@@ -11,3 +14,13 @@ data class MovieCastDto(
     @SerializedName("id")
     val id: Int
 )
+
+fun MovieCastDto.toCast() : List<Cast> {
+    return cast.map {
+        Cast(
+            name = it.name,
+            character = it.character,
+            profilePath = it.profilePath
+        )
+    }
+}
---
+++ app/src/main/java/com/bima/movieapp/domain/model/Cast.kt
@@ -0,0 +1,7 @@
+package com.bima.movieapp.domain.model
+
+data class Cast(
+    val name : String,
+    val character: String,
+    val profilePath: String
+)
