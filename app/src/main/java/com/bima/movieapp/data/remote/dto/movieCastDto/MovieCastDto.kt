6fc202b9fb304b9c9ab06cd341feef632e374433
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-19 14:32:42 +0700

    add movie cast dto

--- app/src/main/java/com/bima/movieapp/data/remote/dto/movieCastDto/MovieCastDto.kt
+++ app/src/main/java/com/bima/movieapp/data/remote/dto/movieCastDto/MovieCastDto.kt
@@ -6,6 +6,8 @@ import com.google.gson.annotations.SerializedName
 data class MovieCastDto(
     @SerializedName("cast")
     val cast: List<Cast>,
+    @SerializedName("crew")
+    val crew: List<Crew>,
     @SerializedName("id")
     val id: Int
 )
