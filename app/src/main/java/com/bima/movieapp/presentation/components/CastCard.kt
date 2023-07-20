72f536a3df496b8372db19503d1e7a799f728589
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-20 18:08:02 +0700

    styling the cast tab

--- app/src/main/java/com/bima/movieapp/presentation/components/CastCard.kt
+++ app/src/main/java/com/bima/movieapp/presentation/components/CastCard.kt
@@ -1,11 +1,21 @@
 package com.bima.movieapp.presentation.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.bima.movieapp.common.Constant
 import com.bima.movieapp.domain.model.Cast
@@ -17,16 +27,22 @@ fun CastCard(
     cast: Cast?,
     modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier.padding(16.dp)
+    Column(
+        modifier = modifier.padding(vertical = 16.dp, horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-       GlideImage(
-           imageModel = { Constant.IMG_URL + cast?.profilePath },
-           imageOptions = ImageOptions(
-               contentScale = ContentScale.Fit
-           )
-       )
-       Text(text = cast?.name.toString())
-       Text(text = cast?.character.toString())
+        GlideImage(
+            imageModel = { Constant.IMG_URL + cast?.profilePath },
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Crop
+            ),
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(120.dp)
+        )
+        Spacer(modifier = modifier.padding(8.dp))
+        Text(text = cast?.name.toString(), fontWeight = FontWeight.SemiBold , style = MaterialTheme.typography.labelLarge)
+        Text(text = cast?.character.toString(), fontStyle = FontStyle.Italic, textAlign = TextAlign.Center, style = MaterialTheme.typography.labelMedium)
     }
+
 }
