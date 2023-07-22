6288ee757ddbd10f6089720c16739e09ec42dc9b
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-22 20:59:10 +0700

    implement carousel

---
+++ app/src/main/java/com/bima/movieapp/presentation/components/DotsIndicator.kt
@@ -0,0 +1,40 @@
+package com.bima.movieapp.presentation.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.primaryContainer /* Color.Yellow */,
+    unSelectedColor: Color = MaterialTheme.colorScheme.tertiaryContainer /* Color.Gray */,
+    dotSize: Dp
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        items(totalDots) { index ->
+            IndicatorDot(
+                color = if (index == selectedIndex) selectedColor else unSelectedColor,
+                size = dotSize
+            )
+
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+            }
+        }
+    }
+}
