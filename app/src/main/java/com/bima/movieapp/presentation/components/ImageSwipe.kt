3adb8b8902d9b559cde0f97e58c1b3c44929eac8
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-07-26 21:42:02 +0700

    add circular loading when state is loading each of component in homescreen

--- app/src/main/java/com/bima/movieapp/presentation/components/ImageSwipe.kt
+++ app/src/main/java/com/bima/movieapp/presentation/components/ImageSwipe.kt
@@ -14,13 +14,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -127,6 +130,18 @@ fun ImageSwipe(
                         navController.navigate(Screen.MovieDetailScreen.route + "/${state.movieList[index].id}")
                     })
                 }
+            }
+            if (state.error.isNotBlank()) {
+                Text(
+                    text = state.error,
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center)
+                )
+            }
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = modifier.align(Alignment.Center))
             }
         }
     }
--- app/src/main/java/com/bima/movieapp/presentation/components/UpcomingMovies.kt
+++ app/src/main/java/com/bima/movieapp/presentation/components/UpcomingMovies.kt
@@ -3,11 +3,16 @@ package com.bima.movieapp.presentation.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -36,6 +41,18 @@ fun UpcomingMovies(
                     }
                 )
             }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                textAlign = TextAlign.Justify,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = modifier.align(Alignment.CenterHorizontally))
         }
     }
 }
