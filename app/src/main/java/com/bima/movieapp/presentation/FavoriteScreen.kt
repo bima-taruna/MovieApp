03cafe6ba8f24e49e5c2eca9581b6a984eeeb021
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-08-07 23:34:10 +0700

    add condition to favorite screen if favorite is empty

--- app/src/main/java/com/bima/movieapp/presentation/FavoriteScreen.kt
+++ app/src/main/java/com/bima/movieapp/presentation/FavoriteScreen.kt
@@ -1,9 +1,13 @@
 package com.bima.movieapp.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bima.movieapp.presentation.components.FavoriteMovieItem
@@ -17,15 +21,18 @@ fun FavoriteScreen(
     navController: NavController
 ) {
     val state = viewModel.state.value
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        items(state.movies.size) { i ->
-            val movie = state.movies[i]
-           FavoriteMovieItem(movieList = movie, onItemClick = {
-               navController.navigate(Screen.MovieDetailScreen.route + "/${movie.id}")
-           })
+
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            items(state.movies.size) { i ->
+                val movie = state.movies[i]
+                FavoriteMovieItem(movieList = movie, onItemClick = {
+                    navController.navigate(Screen.MovieDetailScreen.route + "/${movie.id}")
+                })
+            }
+        }
+        if(state.movies.isEmpty()) {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                Text(text = "Favorite Movies is Empty", textAlign = TextAlign.Center)
+            }
         }
     }
-
-}
