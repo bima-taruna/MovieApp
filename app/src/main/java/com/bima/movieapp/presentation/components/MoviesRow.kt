15757bfb8cc2c01a1c8dfaf9e250ab5890b891ba
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-09-05 17:54:21 +0700

    clean import

--- app/src/main/java/com/bima/movieapp/presentation/components/MoviesRow.kt
+++ app/src/main/java/com/bima/movieapp/presentation/components/MoviesRow.kt
@@ -1,26 +1,17 @@
 package com.bima.movieapp.presentation.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.bima.movieapp.common.Constant
 import com.bima.movieapp.domain.model.MovieList
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun MoviesRow(
