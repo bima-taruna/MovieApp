934c971a44f9fa9d1fb38b4f0d94fce8603cd5cf
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-09-09 17:39:58 +0700

    responsive = fix the add to wishlist button styling

--- app/src/main/java/com/bima/movieapp/presentation/components/BigButton.kt
+++ app/src/main/java/com/bima/movieapp/presentation/components/BigButton.kt
@@ -1,10 +1,12 @@
 package com.bima.movieapp.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -14,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,7 +28,9 @@ fun BigButton(
 ) {
     Button(
         modifier = modifier
-            .padding(8.dp),
+            .padding(8.dp)
+            .width(IntrinsicSize.Min)
+        ,
         shape = RoundedCornerShape(8.dp),
         onClick = onClick
     ) {
@@ -38,10 +43,10 @@ fun BigButton(
                 Icon(
                     imageVector = icon,
                     contentDescription = null,
-                    modifier.size(18.dp)
+                    modifier.weight(1f).size(18.dp)
                 )
             }
-            Text(text = buttonText,  fontWeight = FontWeight.Bold, )
+            Text(text = buttonText,  fontWeight = FontWeight.Bold, textAlign = TextAlign.Center, modifier = Modifier.weight(3f))
         }
     }
 }
