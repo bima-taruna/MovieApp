186337041eb56e768edeae520053aaac7e446fb9
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-09-11 16:50:51 +0700

    dark mode implemented

--- app/src/main/java/com/bima/movieapp/presentation/SettingsScreen.kt
+++ app/src/main/java/com/bima/movieapp/presentation/SettingsScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bima.movieapp.viewmodel.UiThemeViewModel
@@ -25,7 +27,7 @@ fun SettingsScreen(
     ) {
         val check by viewModel.getUiSettings().collectAsStateWithLifecycle(initialValue = false)
         Row(
-            modifier.fillMaxWidth(),
+            modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
